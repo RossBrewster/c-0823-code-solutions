46ee894eafbb364246a1fac3aa46e59d822a7073
Ross Brewster <ross.brewster14@gmail.com>
2023-08-28 21:52:38 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+const width = 20;
+const height = 10;
+
+const area = width * height;
+console.log('Area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 200;
+const payment = 250;
+const change = payment - bill;
+console.log('Change', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 80;
+const midterm = 90;
+const final = 100;
+const grade = (quizzes + midterm + final) / 3;
+console.log('Grade:', grade);
+console.log('typeof grade', typeof grade);
+
+const firstName = 'Ross';
+const lastName = 'Brewster';
+const fullName = firstName + lastName;
+console.log('Fullname:', fullName);
+console.log('typeof Fullname:', typeof fullName);
+
+const pH = 9;
+const isAcidic = pH < 7;
+console.log('9 is acidic.', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+const headCount = 12;
+const isSparta = headCount === 300;
+console.log('isSparta;', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto += ' is the GOAT';
+console.log('motto:', motto);
+console.log('typeof motto:', typeof motto);
