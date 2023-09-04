8cd7e056ef2fc09a95447164b591caeb7e03fae3
Ross Brewster <ross.brewster14@gmail.com>
2023-09-04 23:42:06 +0000

    Progress on array-manipulation-2

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,8 @@
 /* exported drop */
+function drop(array, count) {
+  const arr = [];
+  for (let i = count; i < array.length; i++) {
+    arr.push(array[i]);
+  }
+  return arr;
+}
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,12 +1,11 @@
 /* exported take */
 function take(array, count) {
   const arr = [];
-  if (array === []) {
-    return arr;
-  } else {
-    for (let i = 0; i < count; i++) {
-      arr.push(array[i]);
-    }
+  if (count > array.length) {
+    return array;
+  }
+  for (let i = 0; i < count; i++) {
+    arr.push(array[i]);
   }
   return arr;
 }
