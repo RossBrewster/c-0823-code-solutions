3ecf8a40c4e3bbb94ca58906b0b79b0c62d5182c
Ross Brewster <ross.brewster14@gmail.com>
2023-09-05 23:52:19 +0000

    started pick.js function.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,2 +1,10 @@
 /* exported pick */
-function pick(source, keys) {}
+function pick(source, keys) {
+  const obj = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] in source) {
+      obj[keys[i]] = source[keys[i]];
+    }
+  }
+  return obj;
+}
