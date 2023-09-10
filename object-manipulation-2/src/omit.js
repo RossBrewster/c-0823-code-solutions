7bdb39aa54868dd9deb59d59e1eab1e38bf54268
Ross Brewster <ross.brewster14@gmail.com>
2023-09-10 03:38:45 +0000

    Assign keys and values to the newObj in omit.js

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,13 @@
 /* exported omit */
+function omit(source, keys) {
+  const newObj = {};
+  for (const key in source) {
+    newObj[key] = source[key];
+  }
+  for (let i = 0; i < keys.length; i++) {
+    if (Object.hasOwn(newObj, keys[i])) {
+      delete newObj[keys[i]];
+    }
+  }
+  return newObj;
+}
