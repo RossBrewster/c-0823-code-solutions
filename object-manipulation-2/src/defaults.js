ea4eb74629739a4f0e3b7238297f3bd94f29330e
Ross Brewster <ross.brewster14@gmail.com>
2023-09-11 17:22:02 +0000

    complete function for invert.js

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,8 @@
 /* exported defaults */
+function defaults(target, source) {
+  for (const key in target) {
+    if (target[source[key]] === undefined) {
+      target[source[key]] = source[key];
+    }
+  }
+}
