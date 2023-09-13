8464582383d3582f05badc3df33fcfcaefa41d4a
Ross Brewster <ross.brewster14@gmail.com>
2023-09-13 18:17:36 +0000

    Completed object-manipulation-2 challenge.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,8 +1,9 @@
 /* exported defaults */
 function defaults(target, source) {
-  for (const key in target) {
-    if (target[source[key]] === undefined) {
-      target[source[key]] = source[key];
+  for (const key in source) {
+    if (Object.hasOwn(target, key) === false) {
+      target[key] = source[key];
     }
   }
+  return target;
 }
