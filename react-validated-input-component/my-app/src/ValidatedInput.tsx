9a8cae4b4eaaf0c14596fda24045891c81fda699
Ross Brewster <ross.brewster14@gmail.com>
2023-10-30 17:36:41 +0000

    add value prop to ValidatedInput component.

--- react-validated-input-component/my-app/src/ValidatedInput.tsx
+++ react-validated-input-component/my-app/src/ValidatedInput.tsx
@@ -50,6 +50,7 @@ export function ValidatedInput() {
           Password
           <input
             type="password"
+            value={password}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
