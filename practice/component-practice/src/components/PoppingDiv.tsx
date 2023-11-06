c8888e08dc546ba1072580dceda923c91cde5f43
Ross Brewster <ross.brewster14@gmail.com>
2023-11-06 05:58:00 +0000

    Add colorful div  component.

---
+++ practice/component-practice/src/components/PoppingDiv.tsx
@@ -0,0 +1,13 @@
+import './PoppingDiv.css';
+
+export function PoppingDiv() {
+  return (
+    <div className="canvas">
+      <div className="outer">
+        <div className="inner">
+          <div className="content"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
