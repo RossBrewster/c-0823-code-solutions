613df1c15387a0c3275fbfb999f0bfd37aeb450b
Ross Brewster <ross.brewster14@gmail.com>
2023-11-06 18:35:04 +0000

    move response.ok check.

--- fetch/main.js
+++ fetch/main.js
@@ -1,11 +1,11 @@
 async function logUsers() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (response.ok === false) {
+      throw new Error(response.status);
+    }
     const result = await response.json();
     console.log(result);
-    if (response.ok === false) {
-      throw new Error('whoops');
-    }
   } catch (Error) {
     console.log(Error);
   }
