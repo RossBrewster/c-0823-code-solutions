771fb4b2a6c9f3c12971d302e4ecef81456db80e
Ross Brewster <ross.brewster14@gmail.com>
2023-11-13 17:07:22 +0000

    Completed stack-2 and queue-2 data structures.

--- data-structures/stacks-2.ts
+++ data-structures/stacks-2.ts
@@ -1,9 +1,37 @@
 import { Stack } from './lib/stack';
 
 export function countValues<T>(stack: Stack<T>): number {
-  return 0;
+  let count = 0;
+  const tempStack = new Stack();
+  while (stack.peek() !== undefined) {
+    const value = stack.pop();
+    if (value !== undefined) {
+      tempStack.push(value);
+      count++;
+    }
+  }
+
+  while (tempStack.peek() !== undefined) {
+    const value = tempStack.pop() as T;
+    if (value) {
+      stack.push(value);
+    }
+  }
+  return count;
 }
 
 export function maxValue(stack: Stack<number>): number {
-  return -Infinity;
+  let max: undefined | number;
+  while (stack.peek() !== undefined) {
+    const current = stack.pop() as number;
+    if (max === undefined) {
+      max = current;
+    } else if (current > max) {
+      max = current;
+    }
+  }
+  if (max === undefined) {
+    return -Infinity;
+  }
+  return max;
 }
